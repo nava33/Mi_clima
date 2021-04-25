9a88b830dfd806497db8e603aee27e7b6fb4ed49
FlutLab.io <>
2021-04-25 01:00:29 +0000

    Initial commit

---
+++ lib/ui/MyHomePage.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:nava/ui/Weather.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: new AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: new Text(widget.title),
+      ),
+      body: Weather(),
+    );
+  }
+}
